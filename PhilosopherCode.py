8d46339a5bc4c0e000ad220d593327f37604c2d4
Nguyễn Xuân Thanh <131781167+ThanhNX0401@users.noreply.github.com>
2023-12-22 07:50:42 +0700

    v2 use fileUIv2 and winUI

--- PhilosopherCode.py
+++ PhilosopherCode.py
@@ -1,13 +1,12 @@
 from PyQt6 import QtCore
-import time
 import sys
-from PyQt6 import QtWidgets,QtCore, QtGui
+from PyQt6 import QtCore, QtGui
 from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt6.QtCore import pyqtSignal
-from fileUI import Ui_MainWindow
+from winUI import Ui_MainWindow
 import threading
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.uic = Ui_MainWindow()
@@ -34,13 +33,14 @@ class MainWindow(QWidget):
         self.uic.Button3.clicked.connect(self.on_Button3_clicked)
         self.uic.Button4.clicked.connect(self.on_Button4_clicked)
         self.uic.Start_button.clicked.connect(self.start)
-        #self.uic.Button0.clicked.connect(lambda: self.philosopher_0_status()) # vao ham chuyen trang thai
-        #self.uic.Button1.clicked.connect(self.philosopher_2_status)
-                
+        
+        self.uic.Reset_button.setEnabled(False)
+        self.uic.Reset_button.clicked.connect(self.reset)
+
     def start(self):
         num_philosophers = 5
         forks = [Fork(i) for i in range(num_philosophers)]
-        self.philosopher_0(forks[0],forks[1])
+        self.philosopher_0(forks[0],forks[1]) #right=0, left=1
         self.philosopher_1(forks[1],forks[2])
         self.philosopher_2(forks[2],forks[3])
         self.philosopher_3(forks[3],forks[4])
@@ -52,6 +52,39 @@ class MainWindow(QWidget):
         self.uic.Button3.setEnabled(True)
         self.uic.Button4.setEnabled(True)
         self.uic.Start_button.setEnabled(False)
+        self.uic.Start_button.setStyleSheet("QPushButton{background-color: red; color: black; border-radius: 5px; font-size: 18px; font-weight: bold;}")
+        self.uic.Reset_button.setEnabled(True)
+        
+    def reset(self):
+        for i in range(5):
+            self.thread[i].stop()
+        self.uic.Button0.setEnabled(False)
+        self.uic.Button1.setEnabled(False)
+        self.uic.Button2.setEnabled(False)
+        self.uic.Button3.setEnabled(False)
+        self.uic.Button4.setEnabled(False)
+        self.uic.Start_button.setEnabled(True)
+        self.uic.Start_button.setStyleSheet("QPushButton {background-color: white; border: 2px solid red; border-radius: 5px; color: red; padding: 5px 10px; font-weight: bold; font-size: 18px;}""QPushButton:hover {background-color: red; color: white;}")
+        self.uic.Button0.setText("Status")
+        self.uic.Button1.setText("Status")
+        self.uic.Button2.setText("Status")
+        self.uic.Button3.setText("Status")
+        self.uic.Button4.setText("Status")
+        self.uic.Button0.setStyleSheet("QPushButton {background-color: white; border: 2px solid blue; border-radius: 5px; color: blue; padding: 5px 10px;font-size: 20px; font-weight: bold;}")
+        self.uic.Button1.setStyleSheet("QPushButton {background-color: white; border: 2px solid blue; border-radius: 5px; color: blue; padding: 5px 10px;font-size: 20px; font-weight: bold;}")
+        self.uic.Button2.setStyleSheet("QPushButton {background-color: white; border: 2px solid blue; border-radius: 5px; color: blue; padding: 5px 10px;font-size: 20px; font-weight: bold;}")
+        self.uic.Button3.setStyleSheet("QPushButton {background-color: white; border: 2px solid blue; border-radius: 5px; color: blue; padding: 5px 10px;font-size: 20px; font-weight: bold;}")
+        self.uic.Button4.setStyleSheet("QPushButton {background-color: white; border: 2px solid blue; border-radius: 5px; color: blue; padding: 5px 10px;font-size: 20px; font-weight: bold;}")
+        self.uic.fork0.move(230, 90)
+        self.uic.fork1.move(360, 170)
+        self.uic.fork2.move(320, 310)
+        self.uic.fork3.move(170, 310)
+        self.uic.fork4.move(120, 190)
+        self.uic.phi1.setPixmap(QtGui.QPixmap(("image/phi_thinking0.jpg")))
+        self.uic.phi2.setPixmap(QtGui.QPixmap(("image/phi_thinking0.jpg")))
+        self.uic.phi3.setPixmap(QtGui.QPixmap(("image/phi_thinking0.jpg")))
+        self.uic.phi4.setPixmap(QtGui.QPixmap(("image/phi_thinking0.jpg")))
+        self.uic.phi5.setPixmap(QtGui.QPixmap(("image/phi_thinking0.jpg")))
         
     def on_Button0_clicked(self):
         self.Button0_event.set()
@@ -85,79 +118,79 @@ class MainWindow(QWidget):
         
         if status=="Thinking":
             button.setEnabled(True)
-            button.setStyleSheet("background-color: green")
+            button.setStyleSheet("QPushButton{background-color: green; border-radius: 5px; font-size: 18px; font-weight: bold; color: white;}""QPushButton:hover {background-color: white; color: green;}")
             button.setText("Thinking")
             face.setPixmap(QtGui.QPixmap(("image/phi_thinking0.jpg")))
             
         elif status=="Waiting":
-            button.setStyleSheet("background-color: yellow")
+            button.setStyleSheet("QPushButton{background-color: yellow; border-radius: 5px; font-size: 18px; font-weight: bold; color: black;}")
             button.setText("Waiting")
             button.setEnabled(False)
             face.setPixmap(QtGui.QPixmap(("image/phi_waiting0.jpg")))
         else:
             button.setEnabled(True)
-            button.setStyleSheet("background-color: red")
+            button.setStyleSheet("QPushButton{background-color: red; border-radius: 5px; font-size: 18px; font-weight: bold; color: white;}""QPushButton:hover {background-color: white; color: red;}")
             button.setText("Eating")
             face.setPixmap(QtGui.QPixmap(("image/phi_eating0.jpg")))
             
             
     def philosopher_0(self,fork0,fork1):
-        self.thread[0] = Philosopher(id=0, left_fork=fork0, right_fork=fork1,button_event=self.Button0_event)
+        self.thread[0] = Philosopher(id=0, left_fork=fork1, right_fork=fork0, button_event=self.Button0_event)
         self.thread[0].start()
         self.thread[0].pick_up_left_fork_signal.connect(self.pick_up_id_0_left)
         self.thread[0].pick_up_right_fork_signal.connect(self.pick_up_id_0_right)
         self.thread[0].put_down_left_fork_signal.connect(self.put_down_id_0_left)
         self.thread[0].put_down_right_fork_signal.connect(self.put_down_id_0_right)
-        self.thread[0].change_status_signal.connect(self.change_status)
-        
-    def pick_up_id_0_left(self):
-        self.uic.fork0.move(300, 80)
+        self.thread[0].change_status_signal.connect(self.change_status)     
+    
     def pick_up_id_0_right(self):
+        self.uic.fork0.move(300, 80)
+    def pick_up_id_0_left(self):
         self.uic.fork1.move(380, 90)
-    def put_down_id_0_left(self):
-        self.uic.fork0.move(230, 90)
     def put_down_id_0_right(self):
+        self.uic.fork0.move(230, 90)
+    def put_down_id_0_left(self):
         self.uic.fork1.move(360, 170)
             
     def philosopher_1(self,fork1,fork2):
-        self.thread[1] = Philosopher(id=1,left_fork=fork1, right_fork=fork2,button_event=self.Button1_event)
+        self.thread[1] = Philosopher(id=1, left_fork=fork2, right_fork=fork1, button_event=self.Button1_event)
         self.thread[1].start()
         self.thread[1].pick_up_left_fork_signal.connect(self.pick_up_id_1_left)
         self.thread[1].pick_up_right_fork_signal.connect(self.pick_up_id_1_right)
         self.thread[1].put_down_left_fork_signal.connect(self.put_down_id_1_left)
         self.thread[1].put_down_right_fork_signal.connect(self.put_down_id_1_right)
         self.thread[1].change_status_signal.connect(self.change_status)
-    def pick_up_id_1_left(self):
-        self.uic.fork1.move(410, 220)
+    
     def pick_up_id_1_right(self):
+        self.uic.fork1.move(410, 220)
+    def pick_up_id_1_left(self):
         self.uic.fork2.move(390, 300)
-    def put_down_id_1_left(self):
-        self.uic.fork1.move(360, 170)
     def put_down_id_1_right(self):
+        self.uic.fork1.move(360, 170)
+    def put_down_id_1_left(self):
         self.uic.fork2.move(320, 300)
 
     
     def philosopher_2(self,fork2,fork3):
-        self.thread[2] = Philosopher(id=2,left_fork=fork2, right_fork=fork3,button_event=self.Button2_event)
+        self.thread[2] = Philosopher(id=2, left_fork=fork3, right_fork=fork2, button_event=self.Button2_event)
         self.thread[2].start()
         self.thread[2].pick_up_left_fork_signal.connect(self.pick_up_id_2_left)
         self.thread[2].pick_up_right_fork_signal.connect(self.pick_up_id_2_right)
         self.thread[2].put_down_left_fork_signal.connect(self.put_down_id_2_left)
         self.thread[2].put_down_right_fork_signal.connect(self.put_down_id_2_right)
         self.thread[2].change_status_signal.connect(self.change_status)
-        #self.thread[2].pick_up_signal.connect(self.my_function)
-        #self.uic.Button2.setEnabled(False)
-    def pick_up_id_2_left(self):
-        self.uic.fork2.move(300, 370)
+        
     def pick_up_id_2_right(self):
+        self.uic.fork2.move(300, 370)
+    def pick_up_id_2_left(self):
         self.uic.fork3.move(210, 370)
-    def put_down_id_2_left(self):
-        self.uic.fork2.move(320, 310)
     def put_down_id_2_right(self):
+        self.uic.fork2.move(320, 310)
+    def put_down_id_2_left(self):
         self.uic.fork3.move(170, 310)
     
     def philosopher_3(self,fork3,fork4):
-        self.thread[3] = Philosopher(id=3,left_fork=fork3, right_fork=fork4,button_event=self.Button3_event)
+        self.thread[3] = Philosopher(id=3, left_fork=fork4, right_fork=fork3, button_event=self.Button3_event)
         self.thread[3].start()
         self.thread[3].pick_up_left_fork_signal.connect(self.pick_up_id_3_left)
         self.thread[3].pick_up_right_fork_signal.connect(self.pick_up_id_3_right)
@@ -165,31 +198,31 @@ class MainWindow(QWidget):
         self.thread[3].put_down_right_fork_signal.connect(self.put_down_id_3_right)
         self.thread[3].change_status_signal.connect(self.change_status)
 
-    def pick_up_id_3_left(self):
-        self.uic.fork3.move(110, 290)
     def pick_up_id_3_right(self):
+        self.uic.fork3.move(110, 290)
+    def pick_up_id_3_left(self):
         self.uic.fork4.move(80, 240)
-    def put_down_id_3_left(self):
-        self.uic.fork3.move(170, 310)
     def put_down_id_3_right(self):
+        self.uic.fork3.move(170, 310)
+    def put_down_id_3_left(self):
         self.uic.fork4.move(120, 190)
     
     def philosopher_4(self,fork4,fork0):
-        self.thread[4] = Philosopher(id=4,left_fork=fork4, right_fork=fork0,button_event=self.Button4_event)
+        self.thread[4] = Philosopher(id=4, left_fork=fork0, right_fork=fork4, button_event=self.Button4_event)
         self.thread[4].start()
         self.thread[4].pick_up_left_fork_signal.connect(self.pick_up_id_4_left)
         self.thread[4].pick_up_right_fork_signal.connect(self.pick_up_id_4_right)
         self.thread[4].put_down_left_fork_signal.connect(self.put_down_id_4_left)
         self.thread[4].put_down_right_fork_signal.connect(self.put_down_id_4_right)
         self.thread[4].change_status_signal.connect(self.change_status)
-
-    def pick_up_id_4_left(self):
-        self.uic.fork4.move(110, 120)
+        
     def pick_up_id_4_right(self):
+        self.uic.fork4.move(110, 120)
+    def pick_up_id_4_left(self):
         self.uic.fork0.move(150, 60)
-    def put_down_id_4_left(self):
-        self.uic.fork4.move(120, 190)
     def put_down_id_4_right(self):
+        self.uic.fork4.move(120, 190)
+    def put_down_id_4_left(self):
         self.uic.fork0.move(230, 90)
     
 
@@ -220,38 +253,59 @@ class Philosopher(QtCore.QThread): #Thread class
         self.left_fork = left_fork
         self.right_fork = right_fork
         self.button_event = button_event
+        self.is_running = True
 
     def run(self):
-        while True:
+        while self.is_running:
+            self.button_event.clear()
             self.status = "Thinking"
             self.change_status_signal.emit(self.status)
-            print(f"Philosopher {self.id} is thinking.")
-            self.button_event.wait()  # Wait for the button signal
+            print(f"Philosopher {self.id + 1} is thinking.")
+            #self.locking.lock()  # Acquire the lock
+            self.button_event.wait()
+            if not self.is_running:  # Check the flag after waiting
+                return# Wait for the button signal
             self.button_event.clear()
+            #self.locking.unlock()
+            
+            #if not self.button_event.isSet():  # Check if the button is already triggered
+            print(f"button {self.id} clicked")
             self.status = "Waiting"  
             self.change_status_signal.emit(self.status)
             
-            print(f"Philosopher {self.id} try to picked up left fork {self.left_fork.fork_id}.")
-            self.left_fork.pick_up()
-            self.pick_up_left_fork_signal.emit(self.left_fork)
-            print(f"Philosopher {self.id} picked up left fork {self.left_fork.fork_id}.")
-            
-            print(f"Philosopher {self.id} try to picked up right fork {self.right_fork.fork_id}.")
+            print(f"Philosopher {self.id + 1} try to pick up right fork {self.right_fork.fork_id}.")
             self.right_fork.pick_up()
             self.pick_up_right_fork_signal.emit(self.right_fork)
-            print(f"Philosopher {self.id} picked up right fork {self.right_fork.fork_id} and is eating.")
+            print(f"Philosopher {self.id + 1} picked up right fork {self.right_fork.fork_id}.")
             
+            print(f"Philosopher {self.id + 1} try to pick up left fork {self.left_fork.fork_id}.")
+            self.left_fork.pick_up()
+            self.pick_up_left_fork_signal.emit(self.left_fork)
+            print(f"Philosopher {self.id + 1} picked up left fork {self.left_fork.fork_id} and is eating.")
+            self.button_event.clear()
             self.status = "Eating"
             self.change_status_signal.emit(self.status)
-            self.button_event.wait()  # Wait for the button signal
+            #self.locking.lock()  # Acquire the lock
+            self.button_event.wait()
+            if not self.is_running:  # Check the flag after waiting
+                return# Wait for the button signal
             self.button_event.clear()
-            
-            self.right_fork.put_down()
-            self.put_down_right_fork_signal.emit(self.right_fork)
-            print(f"Philosopher {self.id} put down right fork {self.right_fork.fork_id}.")
-            self.left_fork.put_down()
+            #self.locking.unlock()
+        
+            #if not self.button_event.isSet():  # Check if the button is already triggered
+            print(f"button {self.id} clicked")
             self.put_down_left_fork_signal.emit(self.left_fork)
-            print(f"Philosopher {self.id} put down left fork {self.left_fork.fork_id}.")
+            self.left_fork.put_down()
+            
+            print(f"Philosopher {self.id + 1} put down left fork {self.left_fork.fork_id}.")
+            self.put_down_right_fork_signal.emit(self.right_fork)
+            self.right_fork.put_down()
+            
+            print(f"Philosopher {self.id + 1} put down right fork {self.right_fork.fork_id}.")
+    
+    def stop(self):
+        self.is_running = False
+        self.button_event.set()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
